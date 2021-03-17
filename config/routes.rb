7e49d8a1a0e19d82599bac9ee71b21475985a622
Kushino-Y <kuskus7856.yi@gmail.com>
2021-03-17 05:50:53 +0000

    show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
-  get 'index' => 'books#index'
+  get 'books' => 'books#index'
+  post 'books' =>'books#create'
+  get 'books/:id' => 'books#show'
 end
